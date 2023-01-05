daa45bf5ebe9fc333a4b027a41e39a59b15d00b4
CoreyRobinsonDev <coreyrob52@gmail.com>
2023-01-05 08:24:40 -0500

    fixed character bug in results component

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
-
 import { useAppSelector } from "../util/hooks";
 import styles from "../styles/results.module.css";
 
 export default function Results() {
   const { wpm, rawWPM, accuracy, correctKeysPressed, keysPressed } = useAppSelector(state => state.duck.type);
-  const initialTime = useAppSelector(state => state.duck.time.initialTime);
-  const originalPrompt = useAppSelector(state => state.duck.text.originalPrompt);
-  const hasReset = useAppSelector(state => state.duck);
-  const [totalPromptLength, setTotalPromptLength] = useState(originalPrompt.length);
-
-  useEffect(() => {
-    setTotalPromptLength(t => t + originalPrompt.length);
-  },[originalPrompt])
-
-  useEffect(() => {
-    if (hasReset) setTotalPromptLength(0);
-  },[hasReset])
+  const { initialTime} = useAppSelector(state => state.duck.time);
+  const { originalPrompt } = useAppSelector(state => state.duck.text);
 
   return <section className={styles.results_section}>
       <div className={styles.results__main}>
@@ -36,7 +24,7 @@ export default function Results() {
         </span>
         <span className={styles.results__result}>
           <p className={styles.results__title}>characters</p>
-          <p className={styles.results__data}>{correctKeysPressed} / {keysPressed} / {totalPromptLength}</p>
+          <p className={styles.results__data}>{correctKeysPressed} / {keysPressed} / {originalPrompt.length}</p>
         </span>
         <span className={styles.results__result}>
           <p className={styles.results__title}>consistency</p>
