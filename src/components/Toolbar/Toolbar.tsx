24f4384c3c9bc8d25f5ffde9b057c631b4236fb8
CoreyRobinsonDev <coreyrob52@gmail.com>
2023-11-04 12:06:43 -0400

    Updated Toolbar component to use state management solution

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -16,13 +16,21 @@ import styles from "./Toolbar.module.css";
 export default function Toolbar() {
     const dispatch = useContext(AppDispatch);
 
+
     return <section className={styles.section}>
+        <ul className={styles.times}>
+            <li onClick={() => dispatch({type: "set_time", payload: 10})}>10</li>
+            <li onClick={() => dispatch({type: "set_time", payload: 30})}>30</li>
+            <li onClick={() => dispatch({type: "set_time", payload: 60})}>60</li>
+            <li onClick={() => dispatch({type: "set_time", payload: 120})}>120</li>
+        </ul>
         <ul className={styles.list}>
             <li>HTML</li>
             <li>CSS</li>
             <li>JavaScript</li>
             <li>TypeScript</li>
             <li>SQL</li>
+            <MdMoreVert />
         </ul>
         <ul className={styles.mobile_list}>
             <TbFileTypeHtml title="HTML" />
@@ -30,7 +38,7 @@ export default function Toolbar() {
             <TbFileTypeJs title="JavaScript" />
             <TbFileTypeTs title="TypeScript" />
             <TbFileTypeSql title="SQL" />
+            <MdMoreVert />
         </ul>
-        <MdMoreVert />
     </section>
 }
